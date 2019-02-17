78eb2b4abb3a1e67f37973a80916f4d0f0807353
Bani Singh <bani.singh@uwaterloo.ca>
2019-02-17 13:14:05 -0500

    Initial Commit

---
+++ Gobblet Gobblers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gobblet Gobblers
+//
+//  Created by Bani Singh on 17/02/19.
+//  Copyright © 2019 Bani Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
